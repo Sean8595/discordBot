31aba28149626a9f693c5756258abdcfc12bd314
Sean Melvin <seanmelvin8595@gmail.com>
2020-08-31 19:51:59 -0400

    test2

--- bot.js
+++ bot.js
@@ -214,7 +214,7 @@ client.on('message', (msg) => {
         })
     function statRoller(modifier){
         var statTotal = 0
-       for (let index = 0; index < array.length; index++) {
+       for (let index = 0; index < 6; index++) {
             singleRoll = Math.floor(Math.random() * 6)
             console.log(singleRoll)
             statTotal + singleRoll == statTotal
