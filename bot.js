29cb4a59577b8c792e9ae0aac827dd389bc1b3b8
Sean Melvin <seanmelvin8595@gmail.com>
2021-06-07 12:44:16 -0400

    fix

--- bot.js
+++ bot.js
@@ -170,18 +170,18 @@ const queueContruct = {
    // Pushing the song to our songs array
    queueContruct.songs.push(song);
    
-   try {
-    // Here we try to join the voicechat and save our connection into our object.
-    var connection = await voiceChannel.join();
-    queueContruct.connection = connection;
-    // Calling the play function to start a song
-    play(message.guild, queueContruct.songs[0]);
-   } catch (err) {
-    // Printing the error message if the bot fails to join the voicechat
-    console.log(err);
-    queue.delete(message.guild.id);
-    return message.channel.send(err);
-   }
+//    try {
+//     // Here we try to join the voicechat and save our connection into our object.
+//     var connection = await voiceChannel.join();
+//     queueContruct.connection = connection;
+//     // Calling the play function to start a song
+//     play(message.guild, queueContruct.songs[0]);
+//    } catch (err) {
+//     // Printing the error message if the bot fails to join the voicechat
+//     console.log(err);
+//     queue.delete(message.guild.id);
+//     return message.channel.send(err);
+//    }
    function play(guild, song) {
     const serverQueue = queue.get(guild.id);
     if (!song) {
